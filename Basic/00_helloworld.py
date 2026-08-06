4467fa856a30f701d5edea85e8aa49ef1fb5f4c2
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-06 12:27:46 -0500

    Agregar archivo 00_helloworld.py con ejemplos de impresión y tipos de datos en Python

---
+++ Basic/00_helloworld.py
@@ -0,0 +1,27 @@
+# Hola mundo
+
+print("Hola Python") # con comillas dobles
+print('Hola Python') # con comillas simples
+
+# Comentarios
+
+"""
+Esto es un
+comentario
+en varias lineaa
+"""
+
+'''
+Esto también es un
+comentario
+en varias lineas
+'''
+
+# Como consultar el tipo de dato
+
+print(type("Soy un dato str")) # Tipo str
+print(type(5)) # Tipo int
+print(type(1.5)) # Tipo float
+print(type(3 + 1j)) # Tipo complex
+print(type(True)) # Tipo bool
+print(type(print("Mi cadena de texto"))) # Tipo NoneType
