2de386c295dfa4df5a770132b12449c7efcf4a41
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-08 10:45:21 -0500

    Agregar archivos de configuración y rutas para la API de usuarios en FastAPI

---
+++ Backend/FastAPI/routes/users.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Entidad user
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    url: str
+    age: int
+
+users_list = [  User(id=1, name="Santiago", surname="Rivera", url="http://rivera.dev", age=21),
+                User(id=2, name="Sinsinati", surname="Dev", url="https://sinsinatidev.com", age=35),
+                User(id=3, name="Irai", surname="Rigoberto", url="https://irei.com", age=20)]
+
+@app.get("/usersjson")
+async def usersjson():
+    return [{"name": "Santiago", "surname": "Rivera", "url": "https://rivera.dev", "age": 21},
+            {"name": "Sinsinati", "surname": "Dev", "url": "https://sinsinatidev.com", "age": 35},
+            {"name": "Irai", "surname": "Rigoberto", "url": "https://irei.com", "age": 20}]
+
+@app.get("/users")
+async def users():
+    return users_list
+
+@app.get("/user/{id}") # Path
+async def user_id(id: int):
+    return search_user(id)
+
+@app.get("/user/") # Query
+async def user(id: int):
+    return search_user(id)
+
+@app.post("/user/")
+async def user_post(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+    return user
+
+@app.put("/user/")
+async def user_put(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error": "No se ha actualizado el usuario"}
+    else:
+        return user
+
+@app.delete("/user/{id}")
+async def user_delete(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+            if saved_user.id == id:
+                del users_list[index]
+                found = True
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+    
+
+def search_user(id: int):
+    users = filter (lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
