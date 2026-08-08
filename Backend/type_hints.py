ebdd428bdb6a9de85ad5517babfa1ac9d47bdd45
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-07 23:15:19 -0500

    Agregar archivo type_hints.py con ejemplos de anotaciones de tipo en Python

---
+++ Backend/type_hints.py
@@ -0,0 +1,17 @@
+#  Type Hints
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str = "My tiped string variable"
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable = 5 # type: ignore
+print(my_typed_variable)
+print(type(my_typed_variable))
