8c9fbed8f9a535af6022f32d510d48e0b08ce179
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-08 23:47:15 -0500

    Agregar implementación de la base de datos de usuarios: incluir modelos, esquemas y enrutadores para manejar operaciones CRUD

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,36 +1,29 @@
 from fastapi import FastAPI
-from routers import products, basic_auth_users, jwt_auth_users, users
+from routers import products, basic_auth_users, jwt_auth_users, users, users_db
 from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
 
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=12475
 app.include_router(products.router)
+
 app.include_router(users.router)
 
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=14094
 app.include_router(basic_auth_users.router)
 
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=17664
 app.include_router(jwt_auth_users.router)
 
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=20480
-# app.include_router(users_db.router)
+app.include_router(users_db.router)
 
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=13618
 app.mount("/static", StaticFiles(directory="static"), name="static") # pyright: ignore[reportGeneralTypeIssues]
 
-
 # Url local: http://127.0.0.1:8000
-
 
 @app.get("/") # type: ignore
 async def root():
     return "Hola FastAPI!"
 
 # Url local: http://127.0.0.1:8000/url
-
 
 @app.get("/url") # type: ignore
 async def url():
