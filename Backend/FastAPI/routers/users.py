5563808bd3b299e8e23c35238add427debc676c8
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-08 21:52:19 -0500

    Corregir error en la función user_post: reemplazar 'rootend' por 'append' para agregar usuarios a la lista

--- Backend/FastAPI/routers/users.py
+++ Backend/FastAPI/routers/users.py
@@ -41,7 +41,7 @@ async def user(id: int):
 async def user_post(user: User):
     if type(search_user(user.id)) == User:
         raise HTTPException(status_code=404, detail="El usuario ya existe")
-    users_list.rootend(user)
+    users_list.append(user)
     return user
 
 @router.put("/user/")
