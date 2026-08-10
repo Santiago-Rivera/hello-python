819d46cc32b38fd1afad3ed2fb32ddda463a1f83
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-10 15:42:54 -0500

    Actualizar la configuración del cliente de MongoDB y agregar python-multipart a los requisitos

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local
+# db_client = MongoClient().local
+
+db_client = MongoClient(
+    "mongodb+srv://santiago:santiago@cluster0.dpbe7sm.mongodb.net/"
+    ).santiago
