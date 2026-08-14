d01e41a4cc5498bac2b8dd12c92531f0725364c1
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-14 11:03:12 -0500

    Agregar función dividir y pruebas unitarias para verificar su comportamiento

---
+++ Testing/test_codigo.py
@@ -0,0 +1,35 @@
+import unittest
+from codigo import dividir
+
+class TestDividir(unittest.TestCase):
+
+    def test_dividir_prositivo(self):
+        self.assertEqual(dividir(10, 2), 5)
+
+    def test_dividir_negativo(self):
+        self.assertEqual(dividir(-10, 2), -5)
+
+    def test_dividir_decimales(self):
+        self.assertEqual(dividir(10, 4), 2.5)
+
+    def test_dividir_cero(self):
+        with self.assertRaises(ValueError):
+            dividir(10, 0)
+
+class TestConexionDummy(unittest.TestCase):
+    def setUp(self):
+        print("\n[setUp] Conectando a la base de datos de pruebas...")
+        self.conexion = "Conectado"
+
+    def test_operacion_uno(self):
+        self.assertEqual(self.conexion, "Conectado")
+
+    def test_operacion_dos(self):
+        self.assertTrue(self.conexion.startswith("Cone"))
+
+    def tearDown(self):
+        print("\n[tearDown] Cerrando la conexion de pruebas...")
+        self.conexion = "Desconectado"
+
+if __name__ == '__main__':
+    unittest.main()
